c96fdf5f84acf2463dc87b4b585672139a65a6a2
ParkHongJ <63193883+ParkHongJ@users.noreply.github.com>
2021-07-01 00:47:55 +0900

    기초패턴 만드는중

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    //총알 속도
+    public float speed;
+    //총알 데미지
+    public int damage;
+    //타겟
+    public Transform target;
+
+}
