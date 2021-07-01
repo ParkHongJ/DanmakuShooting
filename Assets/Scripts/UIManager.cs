2613f295931176cdf222a19278951bfbb8276320
eunseo <rhkrdmstj01@naver.com>
2021-07-01 19:36:35 +0900

    UI 기본틀

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject MainPage;
+    public GameObject ControlMethod_click;
+    public GameObject Option_click;
+    public GameObject Play_click;
+    public GameObject Quit_click;
+    public GameObject PlayingUI;
+    public GameObject PlayerConversation;
+
+    public void ControlMethod_Button()
+    {
+        ControlMethod_click.SetActive(true);
+    }
+
+    public void Option_Button()
+    {
+        Option_click.SetActive(true);
+    }
+
+    public void Play_Button()
+    {
+        Play_click.SetActive(true);
+    }
+
+    public void Quit_Button()
+    {
+        Quit_click.SetActive(true);
+    }
+
+    public void ControlMethod_X()
+    {
+        ControlMethod_click.SetActive(false);
+    }
+
+    public void Option_X()
+    {
+        Option_click.SetActive(false);
+    }
+
+    public void Play_X()
+    {
+        Play_click.SetActive(false);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
