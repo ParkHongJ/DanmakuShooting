18db5ce37015a9a3127c59fa8ef18f3a5294996b
ParkHongJ <63193883+ParkHongJ@users.noreply.github.com>
2021-07-03 03:15:30 +0900

    샷건? 같이 쏘는거 만드는중

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,18 +8,41 @@ public class Enemy : MonoBehaviour
     public GameObject player;
     public Transform firePos;
     public float bulletSpeed = 10;
-    // Update is called once per frame
+    public GameObject[] bullets;
+
+    //private void Awake()
+    //{
+    //    for (int i = 0; i < 200; i++)
+    //    {
+    //        bullets[i] = Instantiate(BulletObj);
+    //    }
+    //}
     void Update()
     {
         Vector3 vec = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
         transform.LookAt(vec);
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject bullet = Instantiate(BulletObj);
-            bullet.SetActive(true);
-            bullet.transform.position = firePos.position;
-            Rigidbody rigid = bullet.GetComponent<Rigidbody>();
-            rigid.AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
+            StartCoroutine(Pattern());
         }
+    }
+    void Fire()
+    {
+        GameObject bullet = Instantiate(BulletObj);
+        bullet.SetActive(true);
+        bullet.transform.position = firePos.position;
+        Rigidbody rigid = bullet.GetComponent<Rigidbody>();
+        rigid.AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
+    }
+    IEnumerator Pattern()
+    {
+        Fire();
+        yield return new WaitForSeconds(.1f);
+        Fire();
+        yield return new WaitForSeconds(.1f);
+        Fire();
+        yield return new WaitForSeconds(.1f);
+        Fire();
+        yield return new WaitForSeconds(.1f);
     }
 }
