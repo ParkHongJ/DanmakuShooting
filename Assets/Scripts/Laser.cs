97bf77f4e781bfa2825109386909e452cdd3d213
ParkHongJ <63193883+ParkHongJ@users.noreply.github.com>
2021-07-14 23:05:50 +0900

    한발쏘기, 4발쏘기, 늪, 레이저 정리해야함

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private LineRenderer lineRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        //라인렌더러 설정
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetColors(Color.red, Color.yellow);
+        lineRenderer.SetWidth(0.1f, 0.1f);
+
+        //라인렌더러 처음위치 나중위치
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, transform.position + new Vector3(0, 10, 0));
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
