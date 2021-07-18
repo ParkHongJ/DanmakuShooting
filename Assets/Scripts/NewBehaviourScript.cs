724b593d51e6a421096d2b8fc333ff324078f62c
ParkHongJ <63193883+ParkHongJ@users.noreply.github.com>
2021-07-18 16:04:12 +0900

    테스트용

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : ActtackPattern
+{
+
+    public Transform firePos;
+    public GameObject BulletObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void Attack()
+    {
+        base.Attack();
+        GameObject bullet = Instantiate(BulletObj);
+        bullet.SetActive(true);
+        bullet.transform.position = firePos.position;
+
+
+    }
+    IEnumerator temp()
+    {
+        Attack();
+        yield return new WaitForSeconds(.2f);
+    }
+}
