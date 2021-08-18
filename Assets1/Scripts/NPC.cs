911fa4a6a6831d4bef519f77154b4336eaabad29
LHJ_1230 <sddlgkrwls00@naver.com>
2021-08-18 11:11:29 +0900

    정리

--- Assets1/Scripts/NPC.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NPC : MonoBehaviour
-{
-    public string situation,  name,npc_name;
-    public int db_num;
-}
