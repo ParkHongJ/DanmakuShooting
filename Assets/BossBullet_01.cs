86de2f8587fec7c14c66f5b4dba7c70a232f312a
ParkHongJ <63193883+ParkHongJ@users.noreply.github.com>
2021-08-19 21:51:30 +0900

    보스AI

---
+++ Assets/BossBullet_01.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBullet_01 : MonoBehaviour
+{
+    public GameObject BulletEffect;
+    public GameObject ExplodedEffect;
+    private void OnTriggerEnter(Collider other)
+    {
+        //총알이펙트를 끄고
+        BulletEffect.SetActive(false);
+        //폭발이펙트를 킴
+        ExplodedEffect.SetActive(true);
+    }
+}
